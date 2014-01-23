eaea7a4e3bda6d62da1059ae05894c38fb603cf0
fajoy <wuminfajoy@gmail.com>
2014-01-23 19:11:04 +0800

    fix openrc.sh.

--- helloclient/openrc.sh
+++ helloclient/openrc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export OS_AUTH_URL=http://127.0.0.1:5000/v2.0
-export OS_TENANT_NAME=
-export OS_USERNAME=
+export OS_TENANT_NAME=demo
+export OS_USERNAME=cyfang
 export OS_HELLO_URL=http://127.0.0.1:10001/v1
 echo "Please enter your OpenStack Password: "
 read -s OS_PASSWORD_INPUT
-
+export OS_PASSWORD=$OS_PASSWORD_INPUT
